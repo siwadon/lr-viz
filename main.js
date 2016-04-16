ee0d0fac11dcb2428f16950706014e4c8965e3b3
Siwadon Saosoong <siwadon.s@gmail.com>
2016-04-16 15:43:59 +0700

    Enable pre-commit with jscs and jshint

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({width: 800, height: 600});
   //mainWIndow.loadURL('file://' + __dirname + '/index.html');
-  
+
   mainWindow.on('closed', function() {
     mainWindow = null;
   });
